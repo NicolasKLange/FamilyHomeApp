0fc2c362c35463b93f8d1eae9cdfa46771bbcc05
Nicolas <nklange986@gmail.com>
2024-12-11 10:24:57 -0300

    adicionando funcionalidade para o usuario esquecer o email do google cadastrado

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,15 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  // Usuário
+  // Usuário atual
   final user = FirebaseAuth.instance.currentUser!;
 
-  //Método de sign out do usuário
-  void signUserOut() {
+  // Fazer sign out do email e senha apenas
+  void signUserOutWithEmailAndPassword() {
     FirebaseAuth.instance.signOut();
+  }
+
+  // Método para esquecer a conta Google cadastrada
+  void forgetAccountWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    try {
+      // Desconectar do Google, se estiver logado
+      if (await googleSignIn.isSignedIn()) {
+        await googleSignIn.signOut();
+      }
+      // Fazer o sign out do Firebase
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      // ignore: avoid_print
+      print("Erro ao deslogar: $e");
+    }
   }
 
   @override
@@ -17,14 +35,32 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
+          // Botão de logout (e-mail e senha)
           IconButton(
-            onPressed: signUserOut,
-            icon: const Icon(Icons.logout),
+            onPressed: signUserOutWithEmailAndPassword,
+            icon: const Icon(Icons.logout_sharp),
           ),
         ],
       ),
       body: Center(
-        child: Text("Logado como " + user.email!),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Logado como " + user.email!),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: forgetAccountWithGoogle,
+              child: Text(
+                "Esquecer e-mail cadastrado",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 16,
+                  decoration: TextDecoration.underline, // Adiciona sublinhado
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
