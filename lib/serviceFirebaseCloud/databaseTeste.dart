2f8e9d805fd142035e6e051b18e44fe355dd0900
Nicolas <nklange986@gmail.com>
2024-12-30 12:58:26 -0300

    add funcionalidade editar e excluir funcionario(teste)

--- lib/serviceFirebaseCloud/databaseTeste.dart
+++ lib/serviceFirebaseCloud/databaseTeste.dart
@@ -13,4 +13,14 @@ class DatabaseMethods {
   Future<Stream<QuerySnapshot>> getEmmployeeDetails()async{
     return await FirebaseFirestore.instance.collection('Employee').snapshots();
   }
+
+  //Função para editar dados do funcionario
+  Future updateEmployeeDetail(String id, Map<String, dynamic> updateInfo) async {
+    return await FirebaseFirestore.instance.collection('Employee').doc(id).update(updateInfo);
+  }
+
+  //Função para deletar funcionario
+  Future deleteEmployeeDetail(String id) async {
+    return await FirebaseFirestore.instance.collection('Employee').doc(id).delete();
+  }
 }
--- lib/testeFirebase/listEmployee.dart
+++ lib/testeFirebase/listEmployee.dart
@@ -3,6 +3,7 @@ import 'package:family_home_app/serviceFirebaseCloud/databaseTeste.dart';
 import 'package:family_home_app/testeFirebase/employee.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ListEmployee extends StatefulWidget {
   const ListEmployee({super.key});
@@ -12,6 +13,9 @@ class ListEmployee extends StatefulWidget {
 }
 
 class _ListEmployeeState extends State<ListEmployee> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
   Stream? EmployeeStream;
 
   getontheload() async {
@@ -50,7 +54,8 @@ class _ListEmployeeState extends State<ListEmployee> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(
                                     'Name : ' + ds['Name'],
@@ -59,7 +64,29 @@ class _ListEmployeeState extends State<ListEmployee> {
                                         fontSize: 20,
                                         fontWeight: FontWeight.bold),
                                   ),
-                                  Icon(Icons.edit, color: Colors.orange,),
+                                  Spacer(),
+                                  GestureDetector(
+                                    onTap: () {
+                                      nameController.text = ds['Name'];
+                                      ageController.text = ds['Age'];
+                                      locationController.text = ds['Location'];
+                                      editEmployeeDetail(ds['Id']);
+                                    },
+                                    child: const Icon(
+                                      Icons.edit,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 5.0,
+                                  ),
+                                  GestureDetector(
+                                    onTap: () async{
+                                      await DatabaseMethods().deleteEmployeeDetail(ds['Id']);
+                                    },
+                                    child: const Icon(Icons.delete,
+                                        color: Colors.orange),
+                                  ),
                                 ],
                               ),
                               Text(
@@ -133,4 +160,135 @@ class _ListEmployeeState extends State<ListEmployee> {
       ),
     );
   }
+
+  //Editar dados funcionario
+  Future editEmployeeDetail(String id) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            content: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(Icons.cancel),
+                      ),
+                      const SizedBox(
+                        width: 60.0,
+                      ),
+                      const Text(
+                        'Edit',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        'Details',
+                        style: TextStyle(
+                            color: Colors.orange,
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Name',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextField(
+                      controller: nameController,
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  // Idade
+                  const Text(
+                    'Age',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextField(
+                      controller: ageController,
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  // Localização
+                  const Text(
+                    'Location',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextField(
+                      controller: locationController,
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        Map<String, dynamic> updateInfo = {
+                          'Name': nameController.text,
+                          'Age': ageController.text,
+                          'Id': id,
+                          'Location': locationController.text,
+                        };
+                        await DatabaseMethods()
+                            .updateEmployeeDetail(id, updateInfo)
+                            .then((value) {
+                          Navigator.pop(context);
+                        });
+                      },
+                      child: const Text('Salvar'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ));
 }
