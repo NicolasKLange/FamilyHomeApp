df6030eeae0dca3a1ea69fc45abad8795e7f6c56
Nicolas <nklange986@gmail.com>
2025-01-31 10:22:39 -0300

    editando tela de tarefas

--- lib/pages/services/tasksList/tasksList.dart
+++ lib/pages/services/tasksList/tasksList.dart
@@ -19,7 +19,10 @@ class _TasksListState extends State<TasksList> {
   final ScrollController _scrollController = ScrollController();
 
   Stream<DocumentSnapshot> get userStream {
-    return FirebaseFirestore.instance.collection('Users').doc(user.uid).snapshots();
+    return FirebaseFirestore.instance
+        .collection('Users')
+        .doc(user.uid)
+        .snapshots();
   }
 
   void _showAddTaskDialog() {
@@ -41,7 +44,8 @@ class _TasksListState extends State<TasksList> {
           TextButton(
             onPressed: () {
               if (taskController.text.isNotEmpty) {
-                DatabaseTasksList.addTask(user.uid, selectedDate, taskController.text);
+                DatabaseTasksList.addTask(
+                    user.uid, selectedDate, taskController.text);
                 taskController.clear();
                 Navigator.pop(context);
               }
@@ -110,29 +114,52 @@ class _TasksListState extends State<TasksList> {
         automaticallyImplyLeading: false,
       ),
       body: Column(
+        
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: () => Navigator.pop(context),
+          const SizedBox(height: 10),
+          Container(
+            margin:
+                const EdgeInsets.only(right: 43, left: 43, top: 30, bottom: 10),
+            padding: const EdgeInsets.only(bottom: 5, top: 5, left: 20),
+            decoration: BoxDecoration(
+              color: const Color(0xFFEDE8E8),
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: const Color(0xFF2B3649),
+                width: 2,
               ),
-              Text(
-                DateFormat("d 'de' MMMM 'de' yyyy", 'pt_BR').format(selectedDate),
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-            ],
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () => Navigator.pop(context),
+                ),
+                const SizedBox(width: 40,),
+                const Text(
+                  ("Tarefas"),
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 15),
+          Text(
+            DateFormat("d 'de' MMMM 'de' yyyy", 'pt_BR').format(selectedDate),
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 15),
+          
           SingleChildScrollView(
-            
             controller: _scrollController,
             scrollDirection: Axis.horizontal,
             child: Row(
               children: List.generate(
                 DateTime(selectedDate.year, selectedDate.month + 1, 0).day,
                 (index) {
-                  DateTime date = DateTime(selectedDate.year, selectedDate.month, index + 1);
+                  DateTime date = DateTime(
+                      selectedDate.year, selectedDate.month, index + 1);
                   bool isToday = date.day == DateTime.now().day;
                   return GestureDetector(
                     onTap: () {
@@ -141,17 +168,19 @@ class _TasksListState extends State<TasksList> {
                       });
                     },
                     child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 4),
-                      padding: EdgeInsets.all(8),
+                      
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      padding: const EdgeInsets.only(top: 17, bottom: 17, left: 15, right: 15),
                       decoration: BoxDecoration(
-                        color: isToday ? Colors.blue : Colors.white,
+                        color: isToday ? const Color(0xff577096) : Colors.white,
                         borderRadius: BorderRadius.circular(10),
-                        border: Border.all(color: Colors.black),
+                        border: Border.all(color: const Color(0xff2B3649)),
                       ),
                       child: Column(
                         children: [
-                          Text(DateFormat('E', 'pt_BR').format(date)),
-                          Text("${date.day}"),
+                          Text(DateFormat('E', 'pt_BR').format(date), style: TextStyle(color: isToday ? Colors.white : const Color(0xff577096), fontWeight: FontWeight.bold),),
+                          Text("${date.day}", style: TextStyle(color: isToday ? Colors.white : const Color(0xff577096), fontWeight: FontWeight.bold), ),
+                          
                         ],
                       ),
                     ),
@@ -165,7 +194,9 @@ class _TasksListState extends State<TasksList> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("Tarefas", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                Text("Tarefas",
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                 IconButton(icon: Icon(Icons.add), onPressed: _showAddTaskDialog)
               ],
             ),
@@ -174,9 +205,11 @@ class _TasksListState extends State<TasksList> {
             child: StreamBuilder(
               stream: DatabaseTasksList.getTasks(user.uid, selectedDate),
               builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+                if (!snapshot.hasData)
+                  return Center(child: CircularProgressIndicator());
                 if (snapshot.data!.docs.isEmpty) {
-                  return Center(child: Text("Não possui nenhuma tarefa neste dia!"));
+                  return Center(
+                      child: Text("Não possui nenhuma tarefa neste dia!"));
                 }
                 return ListView(
                   children: snapshot.data!.docs.map((doc) {
@@ -184,13 +217,15 @@ class _TasksListState extends State<TasksList> {
                       leading: Checkbox(
                         value: doc['completed'],
                         onChanged: (value) {
-                          DatabaseTasksList.toggleTaskCompletion(user.uid, selectedDate, doc.id, doc['completed']);
+                          DatabaseTasksList.toggleTaskCompletion(
+                              user.uid, selectedDate, doc.id, doc['completed']);
                         },
                       ),
                       title: Text(doc['description']),
                       trailing: IconButton(
                         icon: Icon(Icons.delete, color: Colors.red),
-                        onPressed: () => DatabaseTasksList.deleteTask(user.uid, selectedDate, doc.id),
+                        onPressed: () => DatabaseTasksList.deleteTask(
+                            user.uid, selectedDate, doc.id),
                       ),
                     );
                   }).toList(),
@@ -202,10 +237,6 @@ class _TasksListState extends State<TasksList> {
       ),
     );
   }
-
-
-
-
 
   void _showOptionsModal(BuildContext context) {
     showModalBottomSheet(
