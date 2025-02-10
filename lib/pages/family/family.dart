4d06b6046e7afb942f51e62260d804fa3f4a08cd
Nicolas <nklange986@gmail.com>
2025-02-10 09:37:43 -0300

    adicionando listView para mostrar todos usuários registrados, para criar família

--- lib/pages/family/family.dart
+++ lib/pages/family/family.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FamilyScreen extends StatelessWidget {
   const FamilyScreen({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
@@ -45,6 +46,53 @@ class FamilyScreen extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('Users').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(
+                      child: Text('Nenhum usuário registrado.'));
+                }
+                var users = snapshot.data!.docs;
+                return ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    var user = users[index];
+                    return Card(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      child: ListTile(
+                        //Avatar do devido usuário
+                        leading: CircleAvatar(
+                          backgroundColor: user['avatarColor'] != null
+                              ? Color(int.parse(user['avatarColor']))
+                              : Colors.grey
+                                  .shade300,
+                          child: Text(
+                            user['name'] != null && user['name'].isNotEmpty
+                                ? user['name'][0].toUpperCase()
+                                : '?', // Inicial do nome
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        //Nome do devido usuário
+                        title: Text(user['name'] ?? 'Usuário sem nome'),
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           ),
         ],
